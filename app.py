a3d0d55da7f04b4b270e3c65d8c9f54a5e82d7cf
Kobert Lam <kobertlam@gmail.com>
2021-12-12 12:02:40 -0500

    completed section 9.4.3

---
+++ app.py
@@ -0,0 +1,10 @@
+# import the dependencies
+from flask import Flask
+
+# Create a new Flask app instance
+app = Flask(__name__)
+
+# Create Flask routes
+@app.route('/')
+def hello_world():
+    return 'Hello world'
